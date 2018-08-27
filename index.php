75669b7bd0665c574de954b13c3a0e249531fec6
dembasy <dembasy1227@gmail.com>
2018-08-27 09:46:11 +0200

    exercice 2 fini

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ex1</title>
+    </head>
+    <body>
+        <p>
+            <?php 
+            // affichage de la date 
+            echo date('d-m-Y');
+            ?>
+        </p>
+    </body>
+</html>
